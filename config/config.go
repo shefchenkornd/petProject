4e1934ad83be1977eb455b0f0eeb6efb587efe14
Ilya Sh <shefchenkornd@gmail.com>
2021-12-18 16:55:42 +0300

    add Config

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+	"sync"
+)
+
+type Config struct {
+	LogLevel            string `envconfig:"LOG_LEVEL"`
+	Debug               string `envconfig:"DEBUG"`
+	HTTPAddr            string `envconfig:"HTTP_ADDR"`
+	PgAddr              string `envconfig:"PG_ADDR"`
+	PgMigrationsPath    string `envconfig:"PG_MIGRATIONS_PATH"`
+	MysqlAddr           string `envconfig:"MYSQL_ADDR"`
+	MysqlUser           string `envconfig:"MYSQL_USER"`
+	MysqlPassword       string `envconfig:"MYSQL_PASSWORD"`
+	MysqlDB             string `envconfig:"MYSQL_DB"`
+	MysqlMigrationsPath string `envconfig:"MYSQL_MIGRATIONS_PATH"`
+}
+
+var (
+	config Config
+	once   sync.Once
+)
+
+// Get reads config from environment. Once.
+func Get() *Config {
+	once.Do(func() {
+		err := envconfig.Process("", &config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configBytes, err := json.MarshalIndent(config, "", " ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Configuration:", string(configBytes))
+	})
+	return &config
+}
