b73ece7a24af953af2e8f1c0195bafd56a81199d
Ilya Sh <shefchenkornd@gmail.com>
2021-12-19 19:24:08 +0300

    add User model

--- model/user.go
+++ model/user.go
@@ -1,6 +1,48 @@
-package user
+package model
 
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// User is a JSON user
 type User struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
+	ID        uuid.UUID `json:"id"`
+	Firstname string    `json:"firstname" validate:"required"`
+	Lastname  string    `json:"lastname" validate:"required"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// ToDB converts User to DBUser
+func (user *User) ToDB() *DBUser {
+	return &DBUser{
+		ID:        user.ID,
+		Firstname: user.Firstname,
+		Lastname:  user.Lastname,
+		CreatedAt: user.CreatedAt,
+	}
+}
+
+// DBUser is a Postgres user
+type DBUser struct {
+	tableName struct{}  `pg:"users" gorm:"primaryKey"`
+	ID        uuid.UUID `pg:"id,notnull,pk"`
+	Firstname string    `pg:"firstname,notnull"`
+	Lastname  string    `pg:"lastname,notnull"`
+	CreatedAt time.Time `pg:"created_at,notnull"`
+}
+
+// TableName overrides default table name for gorm
+func (DBUser) TableName() string {
+	return "users"
+}
+
+// ToWeb converts DBUser to User
+func (dbUser *DBUser) ToWeb() *User {
+	return &User{
+		ID:        dbUser.ID,
+		Firstname: dbUser.Firstname,
+		Lastname:  dbUser.Lastname,
+		CreatedAt: dbUser.CreatedAt,
+	}
 }
