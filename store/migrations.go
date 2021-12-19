1f90e949b3e6ba33506e602fe3115f5ba08227f9
Ilya Sh <shefchenkornd@gmail.com>
2021-12-19 18:48:02 +0300

    add func runMysqlMigrations()

---
+++ store/migrations.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/pkg/errors"
+	"petProject/config"
+)
+
+// runMysqlMigrations runs MySQL migrations
+func runMysqlMigrations() error {
+	cfg := config.Get()
+	if cfg.MysqlMigrationsPath == "" {
+		return nil
+	}
+	if cfg.MysqlDB == "" {
+		return errors.New("No cfg.MysqlDB provided")
+	}
+
+	m, err := migrate.New(
+		cfg.MysqlMigrationsPath,
+		fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s", cfg.MysqlUser, cfg.MysqlPassword, cfg.MysqlAddr, cfg.MysqlDB),
+	)
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
