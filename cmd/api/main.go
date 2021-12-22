e3f241107e042d87d333ebb68a31c128dc5c7a42
Ilya Sh <shefchenkornd@gmail.com>
2021-12-22 18:13:19 +0300

    draft version of main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,42 +3,57 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"log"
-	"net/http"
-	user "petProject/model"
+	"context"
+	"petProject/config"
+	"petProject/controller"
+	"petProject/logger"
+	"petProject/service"
 )
 
 func main() {
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
-
-	e := echo.New()
-
-	// Routing
-	g := e.Group("/api")
-	g.GET("/users", getUsers)
-	//g.GET("/users/:id", getUser)
-	g.POST("/users", saveUser)
-	//g.PUT("/users/:id", updateUser)
-	//g.DELETE("/users/:id", deleteUser)
-
-	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func run() error {
+	ctx := context.Background()
 
-	return nil
-}
+	// config
+	cfg := config.Get()
 
-func getUsers(c echo.Context) error {
-	return c.String(http.StatusOK, "all users!")
-}
+	// logger
+	l := logger.Get()
 
-func saveUser(c echo.Context) error {
-	u := new(user.User)
-	if err := c.Bind(u); err != nil {
-		return err
+	// Init repository store (with mysql/postgresql inside)
+	//store, err := store.New(ctx)
+	//if err != nil {
+	//	return errors.New(err)
+	//}
+
+	// Init service manager
+	serviceManager, err := service.NewManager(ctx, store)
+	if err != nil {
+		errors.New("manager.New failed")
 	}
 
-	return c.JSON(http.StatusCreated, u)
+	// Init controllers
+	userController := controller.NewUsers(ctx, serviceManager, l)
+
+	e := echo.New()
+
+	// API V1
+	v1 := e.Group("/v1")
+
+	// User routes
+	userRoutes := v1.Group("/users")
+	userRoutes.GET("/users", userController.Get)
+	//v1.GET("/users/:id", getUser)
+	userRoutes.POST("/users", userController.Create)
+	//v1.PUT("/users/:id", updateUser)
+	//v1.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+	return nil
 }
