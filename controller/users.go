808f64977d71c19af624f9e724336955db6074b4
Ilya Sh <shefchenkornd@gmail.com>
2021-12-26 12:15:38 +0300

    finished controller/users.go

--- controller/users.go
+++ controller/users.go
@@ -2,9 +2,13 @@ package controller
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 	"net/http"
+	"petProject/lib/types"
 	"petProject/logger"
+	"petProject/model"
 	"petProject/service"
 )
 
@@ -18,21 +22,88 @@ type UserController struct {
 func NewUsers(ctx context.Context, service *service.Manager, logger *logger.Logger) *UserController {
 	return &UserController{
 		ctx: ctx,
-		service: service,
+		services: service,
 		logger: logger,
 	}
 }
 
 
 func (c UserController) Get(ctx echo.Context) error {
-	c.services.GetUser()
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, errors.Wrap(err, "could not decode user data"))
+	}
 
-	return ctx.JSON(http.StatusOK, "all users!")
+	user, err := c.services.User.GetUser(ctx.Request().Context(), id)
+	if err != nil {
+		switch {
+		case errors.Cause(err) == types.ErrNotFound:
+			return echo.NewHTTPError(http.StatusNotFound, err)
+		case errors.Cause(err) == types.ErrBadRequest:
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		default:
+			return echo.NewHTTPError(http.StatusInternalServerError, errors.Wrap(err, "could not get user"))
+		}
+		return echo.NewHTTPError(http.StatusOK, user)
+	}
+
+	return ctx.JSON(http.StatusOK,  user)
 }
 
 func (c UserController) Create(ctx echo.Context) error {
-	c.services.CreateUser()
+	var user model.User
+	err := ctx.Bind(&user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, errors.Wrap(err, "could not decode user data"))
+	}
 
+	createdUser, err := c.services.User.CreateUser(ctx.Request().Context(), &user)
+	if err != nil {
+		switch {
+		case errors.Cause(err) == types.ErrBadRequest:
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		default:
+			return echo.NewHTTPError(http.StatusInternalServerError, errors.Wrap(err, "could not create user"))
+		}
+	}
 
-	return ctx.JSON(http.StatusOK, "all users!")
+	c.logger.Debug().Msgf("Created user '%s'", createdUser.ID.String())
+
+	return ctx.JSON(http.StatusOK, createdUser)
+}
+
+func (c UserController) Update(ctx echo.Context) error {
+	var user model.User
+	err := ctx.Bind(user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, errors.Wrap(err, "could not decode user data"))
+	}
+
+	updatedUser, err := c.services.User.UpdateUser(ctx.Request().Context(), &user)
+	if err != nil {
+		switch {
+		case errors.Cause(err) == types.ErrBadRequest:
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		default:
+			return echo.NewHTTPError(http.StatusInternalServerError, errors.Wrap(err, "could not create user"))
+		}
+	}
+
+	c.logger.Debug().Msgf("Updated user '%s'", updatedUser.ID.String())
+
+	return ctx.JSON(http.StatusOK, updatedUser)
+}
+
+func (c UserController) Delete(ctx echo.Context) error {
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, errors.Wrap(err, "could not delete user"))
+	}
+
+	err = c.services.User.DeleteUser(ctx.Request().Context(), id)
+	if err != nil {
+
+	}
+
+	return ctx.JSON(http.StatusOK, model.OK)
 }
